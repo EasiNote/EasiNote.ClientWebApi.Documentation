2e92a42612ac913b3e100cb7379c73c6bef4d8ec
Jasongrass <junjiegrass@qq.com>
2021-02-28 11:51:53 +0800

    修改 icon 和标题

--- docs-src/vue.config.js
+++ docs-src/vue.config.js
@@ -1,4 +1,11 @@
 // vue.config.js
 module.exports = {
-    publicPath: './'
+    publicPath: './',
+    chainWebpack: config =>{
+        config.plugin('html')
+          .tap(args => {
+            args[0].title = "EN web 回调测试";
+            return args;
+          })
+      },
 }
