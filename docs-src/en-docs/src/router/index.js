c305d411363fae8318e0d0c7a355bc6530c07c6a
Jasongrass <junjiegrass@qq.com>
2021-02-28 11:20:58 +0800

    添加 DebugPage3

--- docs-src/en-docs/src/router/index.js
+++ docs-src/en-docs/src/router/index.js
@@ -6,6 +6,8 @@ Vue.use(VueRouter)
 
 const DebugPage1 = () => import('../components/DebugPage1.vue')
 const DebugPage2 = () => import('../components/DebugPage2.vue')
+const DebugPage3 = () => import('../components/DebugPage3.vue')
+
 
 const routes = [{
     path: '/',
@@ -19,6 +21,10 @@ const routes = [{
   {
     path: "/debugPage2",
     component: DebugPage2
+  },
+  {
+    path: "/debugPage3",
+    component: DebugPage3
   }
 
 ]
