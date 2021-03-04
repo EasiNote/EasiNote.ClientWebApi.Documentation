14e066f114466cf92c6b71fe3da23bc2df3af5c6
Jasongrass <junjiegrass@qq.com>
2021-03-04 14:58:57 +0800

    添加插入元素的调用形式

--- docs-src/src/components/DebugPage1.vue
+++ docs-src/src/components/DebugPage1.vue
@@ -1,94 +1,153 @@
 <template>
-  <div class="grid-content">
+  <div>
     <h3>1 执行通用回调 InvokeMethod</h3>
-    <p>
-      window.external.InvokeMethod(JSON.stringify({ "method": "实际方法名",
-      "args": JSON.stringify({ "参数1": "值1", "参数2": "值2", "参数3": "值3"
-      })}))
-    </p>
+    <div style="margin-bottom:1em">
+      <el-link
+        type="primary"
+        target="_blank"
+        href="https://github.com/EasiNote/EasiNote.ClientWebApi.Documentation/blob/test/zh-CN/apis/README.md"
+      >
+        相关文档
+      </el-link>
+    </div>
 
-    <el-link
-      type="primary"
-      target="_blank"
-      href="https://github.com/EasiNote/EasiNote.ClientWebApi.Documentation/blob/test/zh-CN/apis/README.md"
-    >
-      相关文档
-    </el-link>
+    <el-button-group>
+      <el-button type="primary" @click="switchToView(0)">默认</el-button>
+      <el-button type="primary" @click="switchToView(1)">插入元素</el-button>
+    </el-button-group>
 
-    <p>将要执行的语句是：{{ resultJsCode }}</p>
+    <div class="grid-content" v-if="invokeType === 0">
+      <p>
+        window.external.InvokeMethod(JSON.stringify({ "method": "实际方法名",
+        "args": JSON.stringify({ "参数1": "值1", "参数2": "值2", "参数3": "值3"
+        })}))
+      </p>
 
-    <h4>方法名</h4>
-    <el-row class="row-bg">
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light">
-          <el-select
-            v-model="commonMethodName"
-            filterable
-            allow-create
-            default-first-option
-            placeholder="请选择或输入方法名"
-          >
-            <el-option
-              v-for="item in methodOptions"
-              :key="item.value"
-              :label="item.label"
-              :value="item.value"
+      <p>将要执行的语句是：{{ resultJsCode }}</p>
+
+      <h4>方法名</h4>
+      <el-row class="row-bg">
+        <el-col :span="4">
+          <div class="grid-content bg-purple-light">
+            <el-select
+              v-model="commonMethodName"
+              filterable
+              allow-create
+              default-first-option
+              placeholder="请选择或输入方法名"
             >
-            </el-option>
-          </el-select>
-        </div>
-      </el-col>
-    </el-row>
+              <el-option
+                v-for="item in methodOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </div>
+        </el-col>
+      </el-row>
 
-    <h4>参数</h4>
+      <h4>参数</h4>
 
-    <el-row class="row-bg" :gutter="20" style="margin-bottom:1em">
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light">
-          <el-input
-            v-model="commonMethodParamName1"
-            placeholder="参数名1"
-          ></el-input>
-        </div>
-      </el-col>
+      <el-row class="row-bg" :gutter="20" style="margin-bottom: 1em">
+        <el-col :span="4">
+          <div class="grid-content bg-purple-light">
+            <el-input
+              v-model="commonMethodParamName1"
+              placeholder="参数名1"
+            ></el-input>
+          </div>
+        </el-col>
 
-      <el-col :span="18">
-        <div class="grid-content bg-purple-light">
-          <el-input
-            type="textarea"
-            :rows="4"
-            v-model="commonMethodParamValue1"
-            placeholder="参数值1"
-          >
-          </el-input>
-        </div>
-      </el-col>
-    </el-row>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            <el-input
+              type="textarea"
+              :rows="4"
+              v-model="commonMethodParamValue1"
+              placeholder="参数值1"
+            >
+            </el-input>
+          </div>
+        </el-col>
+      </el-row>
 
-    <el-row class="row-bg" :gutter='20'>
-      <el-col :span="4">
-        <div class="grid-content bg-purple-light">
-          <el-input
-            v-model="commonMethodParamName2"
-            placeholder="参数名2"
-          ></el-input>
-        </div>
-      </el-col>
+      <el-row class="row-bg" :gutter="20">
+        <el-col :span="4">
+          <div class="grid-content bg-purple-light">
+            <el-input
+              v-model="commonMethodParamName2"
+              placeholder="参数名2"
+            ></el-input>
+          </div>
+        </el-col>
 
-      <el-col :span="18">
-        <div class="grid-content bg-purple-light">
-          <el-input
-            type="textarea"
-            :rows="4"
-            v-model="commonMethodParamValue2"
-            placeholder="参数值2"
-          >
-          </el-input>
-        </div>
-      </el-col>
-    </el-row>
+        <el-col :span="18">
+          <div class="grid-content bg-purple-light">
+            <el-input
+              type="textarea"
+              :rows="4"
+              v-model="commonMethodParamValue2"
+              placeholder="参数值2"
+            >
+            </el-input>
+          </div>
+        </el-col>
+      </el-row>
 
-    <el-button type="primary" @click="invokeCommonMethod">执行</el-button>
+      <el-button type="primary" @click="invokeCommonMethod">执行</el-button>
+    </div>
+
+    <div class="grid-content" v-if="invokeType === 1">
+      <p>
+        window.external.InvokeMethod(JSON.stringify({ "method":
+        "实际方法名","args": "参数值" )}))
+      </p>
+
+      <p>将要执行的语句是：{{ resultJsCode2 }}</p>
+
+      <h4>方法名</h4>
+      <el-row class="row-bg">
+        <el-col :span="4">
+          <div class="grid-content bg-purple-light">
+            <el-select
+              v-model="commonMethodName"
+              filterable
+              allow-create
+              default-first-option
+              placeholder="请选择或输入方法名"
+            >
+              <el-option
+                v-for="item in methodOptions"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              >
+              </el-option>
+            </el-select>
+          </div>
+        </el-col>
+      </el-row>
+
+      <h4>参数</h4>
+
+      <el-row class="row-bg" :gutter="22" style="margin-bottom: 1em">
+        <el-col :span="22">
+          <div class="grid-content bg-purple-light">
+            <el-input
+              type="textarea"
+              :rows="8"
+              v-model="commonMethodParamValue1"
+              placeholder="参数值"
+            >
+            </el-input>
+          </div>
+        </el-col>
+      </el-row>
+
+      <el-button type="primary" @click="invokeCommonMethod2">执行</el-button>
+    </div>
   </div>
 </template>
 
@@ -98,6 +157,7 @@ export default {
 
   data() {
     return {
+      invokeType: 0,
       commonMethodName: "",
       commonMethodParamName1: "",
       commonMethodParamValue1: "",
@@ -122,6 +182,10 @@ export default {
       return `window.external.InvokeMethod("${this.commonInvokeMethodJson}")`;
     },
 
+    resultJsCode2: function () {
+      return `window.external.InvokeMethod("${this.commonInvokeMethodJson2}")`;
+    },
+
     commonInvokeMethodJson: function () {
       let methodName = this.commonMethodName;
       let pn1 = this.commonMethodParamName1;
@@ -138,11 +202,27 @@ export default {
         args: params,
       });
     },
+
+    commonInvokeMethodJson2: function () {
+      let methodName = this.commonMethodName;
+      let param = this.commonMethodParamValue1;
+
+      return JSON.stringify({
+        method: methodName,
+        args: param,
+      });
+    },
   },
 
   methods: {
     invokeCommonMethod() {
       window.external.InvokeMethod(this.commonInvokeMethodJson);
+    },
+    invokeCommonMethod2() {
+      window.external.InvokeMethod(this.commonInvokeMethodJson2);
+    },
+    switchToView(index) {
+      this.invokeType = index;
     },
   },
 };
