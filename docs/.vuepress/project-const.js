bc443a6c65c0f3f2a83f0d4ee30bd0fecddfb5ac
Jasongrass <junjiegrass@qq.com>
2021-04-12 16:01:08 +0800

    添加部署文档

--- docs/.vuepress/project-const.js
+++ docs/.vuepress/project-const.js
@@ -2,6 +2,8 @@ module.exports = {
     // 站点的根路径
     // github: /EasiNote.ClientWebApi.Documentation/
     // gitee: /client-web-api-documentation/
+
     base: '/EasiNote.ClientWebApi.Documentation/',
+    //base: '/client-web-api-documentation/',
 }
 
