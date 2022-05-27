4e2e8384c927a53006eae7972dfd2e784bc635ac
Mcxunyun666 <51842260+Mcxunyun666@users.noreply.github.com>
2022-05-27 23:52:33 +0800

    对应vercel根路径
    添加 /EasiNote.ClientWebApi.Doc.VercelMirror/ 为vercel部署根路径

---
+++ deploy/basePath.vercel.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // 站点的根路径
+    // github: /EasiNote.ClientWebApi.Documentation/
+    // gitee: /client-web-api-documentation/
+    // vercel: /EasiNote.ClientWebApi.Doc.VercelMirror/
+
+    base: '/EasiNote.ClientWebApi.Doc.VercelMirror/',
+    //base: '/EasiNote.ClientWebApi.Documentation/',
+    //base: '/client-web-api-documentation/',
+}
