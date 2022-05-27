3cb2183d1fcd17aabe7ce5386a5aee0c6c525313
Mcxunyun666 <51842260+Mcxunyun666@users.noreply.github.com>
2022-05-28 00:06:47 +0800

    取消路径定向

--- deploy/basePath.vercel.js
+++ deploy/basePath.vercel.js
@@ -2,7 +2,7 @@ module.exports = {
     // 站点的根路径
     // github: /EasiNote.ClientWebApi.Documentation/
     // gitee: /client-web-api-documentation/
-    // vercel: /EasiNote.ClientWebApi.Doc.VercelMirror/
+    // vercel: /
 
     base: '/EasiNote.ClientWebApi.Doc.VercelMirror/',
     //base: '/EasiNote.ClientWebApi.Documentation/',
